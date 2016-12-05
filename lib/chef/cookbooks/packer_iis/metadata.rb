d8f9cf66ce98da083bfba529d3096190d6599ea5
Gusztáv Varga <me@gusztavvargadr.me>
2016-12-05 15:01:08 +0100

    Add missing metadata and Chef ignore settings #31

--- lib/chef/cookbooks/packer_iis/metadata.rb
+++ lib/chef/cookbooks/packer_iis/metadata.rb
@@ -1,3 +1,9 @@
 name 'packer_iis'
+maintainer 'Gusztav Varga'
+maintainer_email 'me@gusztavvargadr.me'
+license 'MIT'
+description 'Installs/Configures IIS'
+long_description 'Installs/Configures IIS'
+version '0.1.0'
 
 depends 'packer_windows'
