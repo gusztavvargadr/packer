90dd9123e95c9ed612c292e8cd292e5ec87fbece
Gusztáv Varga <me@gusztavvargadr.me>
2017-05-23 14:30:02 +0200

    Update download urls to latest

--- src/core/chef/cookbooks/gusztavvargadr_packer_vs/attributes/default.rb
+++ src/core/chef/cookbooks/gusztavvargadr_packer_vs/attributes/default.rb
@@ -2,10 +2,11 @@ default['gusztavvargadr_packer_vs']['2010_professional']['installer_file_url'] =
 default['gusztavvargadr_packer_vs']['2010_sp1']['installer_file_url'] = 'https://onedrive.live.com/download?cid=EFF41AD5731FA3EA&resid=EFF41AD5731FA3EA%21706721&authkey=AOu9U-4vRvodxyM'
 default['gusztavvargadr_packer_vs']['2015_community']['installer_file_url'] = 'https://onedrive.live.com/download?cid=EFF41AD5731FA3EA&resid=EFF41AD5731FA3EA%21706722&authkey=ACa69dL0LNU0GiM'
 default['gusztavvargadr_packer_vs']['2015_professional']['installer_file_url'] = 'https://onedrive.live.com/download?cid=EFF41AD5731FA3EA&resid=EFF41AD5731FA3EA%21706720&authkey=AP9C3_NOYmJxU9A'
-default['gusztavvargadr_packer_vs']['2017_community']['installer_file_url'] = 'https://download.microsoft.com/download/A/6/B/A6B03A5D-808D-4E21-9CF7-532D16240AD7/vs_Community.exe'
+default['gusztavvargadr_packer_vs']['2017_community']['installer_file_url'] = 'https://download.microsoft.com/download/4/3/3/433CE112-6BA4-49A1-A59F-E15F01E7B851/vs_Community.exe'
 default['gusztavvargadr_packer_vs']['2017_community']['installer_options'] = [
   '--installPath ""C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Community""',
   '--add Microsoft.VisualStudio.Workload.CoreEditor',
+  '--add Microsoft.VisualStudio.Workload.ManagedDesktop',
   '--add Microsoft.VisualStudio.Workload.NetWeb',
   '--add Microsoft.VisualStudio.Workload.Data',
   '--add Microsoft.VisualStudio.Workload.Azure',
@@ -15,12 +16,13 @@ default['gusztavvargadr_packer_vs']['2017_community']['installer_options'] = [
   '--add Component.GitHub.VisualStudio',
   '--includeRecommended',
   '--includeOptional',
-  '--quiet --norestart'
+  '--quiet --norestart',
 ]
-default['gusztavvargadr_packer_vs']['2017_professional']['installer_file_url'] = 'https://download.microsoft.com/download/7/B/0/7B0534FD-DCE5-46D5-8771-91FD32AFC51D/vs_Professional.exe'
+default['gusztavvargadr_packer_vs']['2017_professional']['installer_file_url'] = 'https://download.microsoft.com/download/D/B/A/DBAB8920-72B8-4EBA-B92C-0EF2D83B0139/vs_Professional.exe'
 default['gusztavvargadr_packer_vs']['2017_professional']['installer_options'] = [
   '--installPath ""C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Professional""',
   '--add Microsoft.VisualStudio.Workload.CoreEditor',
+  '--add Microsoft.VisualStudio.Workload.ManagedDesktop',
   '--add Microsoft.VisualStudio.Workload.NetWeb',
   '--add Microsoft.VisualStudio.Workload.Data',
   '--add Microsoft.VisualStudio.Workload.Azure',
@@ -30,5 +32,5 @@ default['gusztavvargadr_packer_vs']['2017_professional']['installer_options'] = 
   '--add Component.GitHub.VisualStudio',
   '--includeRecommended',
   '--includeOptional',
-  '--quiet --norestart'
+  '--quiet --norestart',
 ]
