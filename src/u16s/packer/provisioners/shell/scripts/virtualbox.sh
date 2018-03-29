1b6d94dbd0e0594cb762baa501bb5800bc0f23aa
Gusztáv Varga <me@gusztavvargadr.me>
2018-03-29 22:53:55 +0200

    Fix VirtualBox builds #81

--- src/u16s/packer/provisioners/shell/scripts/virtualbox.sh
+++ src/u16s/packer/provisioners/shell/scripts/virtualbox.sh
@@ -7,6 +7,7 @@ case "$PACKER_BUILDER_TYPE" in
 virtualbox-iso|virtualbox-ovf)
     VER="`cat $HOME_DIR/.vbox_version`";
     ISO="VBoxGuestAdditions_$VER.iso";
+    wget http://download.virtualbox.org/virtualbox/$VER/$ISO
     mkdir -p /tmp/vbox;
     mount -o loop $HOME_DIR/$ISO /tmp/vbox;
     sh /tmp/vbox/VBoxLinuxAdditions.run \
