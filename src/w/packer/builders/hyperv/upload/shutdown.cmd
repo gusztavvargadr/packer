870826f76094c5e49bbd396cb5570d46129e72ec
Gusztáv Varga <mail@gusztavvargadr.me>
2019-07-09 10:56:15 +0200

    Pause before Sysprep temporarily #51

--- src/w/packer/builders/hyperv/upload/shutdown.cmd
+++ src/w/packer/builders/hyperv/upload/shutdown.cmd
@@ -1,4 +1,5 @@
 sc.exe config winrm start= disabled
 sc.exe config sshd start= disabled
 
+timeout 180
 C:\Windows\System32\Sysprep\sysprep.exe /generalize /oobe /quiet /shutdown /unattend:C:\Windows\Setup\packer\Autounattend.xml
