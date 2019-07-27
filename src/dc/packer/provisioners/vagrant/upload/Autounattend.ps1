25eae98fd043fb4b83244bcfc0967bfd3553ebbb
Gusztáv Varga <mail@gusztavvargadr.me>
2019-07-27 20:31:53 +0200

    Update and fix Docker provisioning #129

--- src/dc/packer/provisioners/vagrant/upload/Autounattend.ps1
+++ src/dc/packer/provisioners/vagrant/upload/Autounattend.ps1
@@ -3,6 +3,7 @@ wmic useraccount where "name='vagrant'" set PasswordExpires=FALSE
 sc.exe config winrm start= auto
 net start winrm
 
+Write-Host "Configure OpenSSH"
 mkdir -Force C:/Users/vagrant/.ssh
 cp C:/Windows/Setup/packer/vagrant.pub C:/Users/vagrant/.ssh/authorized_keys
 
