6a4db3f21d5361a8d4cc654d838bd24d908a30ab
Gusztáv Varga <mail@gusztavvargadr.me>
2019-09-14 17:45:27 +0200

    Move servicing update to install stage #139

--- src/ws2016s/chef/cookbooks/gusztavvargadr_packer_ws2016s/recipes/prepare.rb
+++ src/ws2016s/chef/cookbooks/gusztavvargadr_packer_ws2016s/recipes/prepare.rb
@@ -1,6 +1,1 @@
 include_recipe 'gusztavvargadr_packer_w::prepare'
-
-gusztavvargadr_windows_updates '' do
-  msu_source 'http://download.windowsupdate.com/c/msdownload/update/software/secu/2019/09/windows10.0-kb4512574-x64_70c6d50fa9b65aa7f7a1202145185f244f8425c7.msu'
-  action :install
-end
