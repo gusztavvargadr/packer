6dc356244a443c44383f1ed7e21c47858580a147
Gusztáv Varga <mail@gusztavvargadr.me>
2019-09-14 18:13:00 +0200

    Remove explicit servicing update and add extra update step #139

--- src/ws2016s/chef/cookbooks/gusztavvargadr_packer_ws2016s/recipes/install.rb
+++ src/ws2016s/chef/cookbooks/gusztavvargadr_packer_ws2016s/recipes/install.rb
@@ -1,6 +1,1 @@
 include_recipe 'gusztavvargadr_packer_w::install'
-
-gusztavvargadr_windows_updates '' do
-  msu_source 'http://download.windowsupdate.com/d/msdownload/update/software/secu/2019/02/windows10.0-kb4485447-x64_e9334a6f18fa0b63c95cd62930a058a51bba9a14.msu'
-  action :install
-end
