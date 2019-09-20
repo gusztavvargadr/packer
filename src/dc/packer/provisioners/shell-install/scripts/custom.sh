1eb311468904dcaf49f948d27fbe227a953f1e50
Gusztáv Varga <mail@gusztavvargadr.me>
2019-09-20 21:29:18 +0200

    Update to latest version of dependencies #141

--- src/dc/packer/provisioners/shell-install/scripts/custom.sh
+++ src/dc/packer/provisioners/shell-install/scripts/custom.sh
@@ -10,11 +10,11 @@ add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(
 apt-get update
 apt-get install -y docker-ce
 
-curl -L https://github.com/docker/machine/releases/download/v0.16.1/docker-machine-$(uname -s)-$(uname -m) -o /tmp/docker-machine
+curl -L https://github.com/docker/machine/releases/download/v0.16.2/docker-machine-$(uname -s)-$(uname -m) -o /tmp/docker-machine
 install /tmp/docker-machine /usr/local/bin/docker-machine
 for i in docker-machine-prompt.bash docker-machine-wrapper.bash docker-machine.bash
 do
-  wget "https://raw.githubusercontent.com/docker/machine/v0.16.1/contrib/completion/bash/${i}" -P /etc/bash_completion.d
+  wget "https://raw.githubusercontent.com/docker/machine/v0.16.2/contrib/completion/bash/${i}" -P /etc/bash_completion.d
 done
 
 curl -L https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
