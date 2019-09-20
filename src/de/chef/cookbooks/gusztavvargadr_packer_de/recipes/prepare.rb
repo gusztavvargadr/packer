dec445d94e26966feca06b82afc652584807c83e
Gusztáv Varga <mail@gusztavvargadr.me>
2019-09-20 08:09:24 +0200

    Change WinRM service to delayed start for Docker Enterprise install #139

--- src/de/chef/cookbooks/gusztavvargadr_packer_de/recipes/prepare.rb
+++ src/de/chef/cookbooks/gusztavvargadr_packer_de/recipes/prepare.rb
@@ -3,3 +3,9 @@ include_recipe 'gusztavvargadr_packer_w::prepare'
 gusztavvargadr_docker_engine 'enterprise' do
   action :prepare
 end
+
+windows_service 'winrm' do
+  action :configure_startup
+  startup_type :automatic
+  delayed_start true
+end
