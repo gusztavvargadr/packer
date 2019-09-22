5c12994921fac1194aec362d0db8b50a9017ea20
Gusztáv Varga <mail@gusztavvargadr.me>
2019-09-22 17:08:27 +0200

    Fix Windows Update and Defender settings #140

--- src/w/packer/provisioners/chef/upload/prepare.ps1
+++ src/w/packer/provisioners/chef/upload/prepare.ps1
@@ -1,3 +1,23 @@
+Write-Host "Disable UAC"
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v EnableLUA /t REG_DWORD /d 0 /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v LocalAccountTokenFilterPolicy /t REG_DWORD /d 1 /f
+
+Write-Host "Disable Windows Updates"
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" /v NoAutoUpdate /d 1 /t REG_DWORD /f /reg:64
+
+Write-Host "Disable Windows Store Updates"
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\WindowsStore" /v AutoDownload /d 2 /t REG_DWORD /f /reg:64
+
+Write-Host "Disable Maintenance"
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\Maintenance" /v MaintenanceDisabled /t REG_DWORD /d 1 /f
+
+Write-Host "Disable Windows Defender"
+Set-MpPreference -DisableRealtimeMonitoring $True
+
+Write-Host "Enable Remote Desktop"
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
+netsh advfirewall firewall add rule name="Remote Desktop" dir=in localport=3389 protocol=TCP action=allow
+
 Write-Host "Install Chef Client"
 choco install chef-client -y --version 15.2.20
 [Environment]::SetEnvironmentVariable("CHEF_LICENSE", "accept-silent", "Machine")
