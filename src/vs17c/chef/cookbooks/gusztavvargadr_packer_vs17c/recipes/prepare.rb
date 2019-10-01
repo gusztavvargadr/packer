d6082f7ae07f7c52e21288a26a4711ad5639af73
Gusztáv Varga <mail@gusztavvargadr.me>
2019-10-01 20:47:57 +0200

    Update dependencies #139

--- src/vs17c/chef/cookbooks/gusztavvargadr_packer_vs17c/recipes/prepare.rb
+++ src/vs17c/chef/cookbooks/gusztavvargadr_packer_vs17c/recipes/prepare.rb
@@ -1,1 +1,2 @@
 include_recipe 'gusztavvargadr_packer_w::prepare'
+include_recipe 'gusztavvargadr_visualstudio::2017_requirements'
--- src/vs19c/chef/cookbooks/gusztavvargadr_packer_vs19c/recipes/prepare.rb
+++ src/vs19c/chef/cookbooks/gusztavvargadr_packer_vs19c/recipes/prepare.rb
@@ -1,1 +1,2 @@
 include_recipe 'gusztavvargadr_packer_w::prepare'
+include_recipe 'gusztavvargadr_visualstudio::2019_requirements'
