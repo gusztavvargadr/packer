37cddeaff5e6f9b26a62e8e657fafb1fcf22ee23
Gusztáv Varga <mail@gusztavvargadr.me>
2019-12-21 22:16:41 +0100

    Consolidate Windows autounattend configuration #1611

--- src/w/packer/builders/iso/floppy/Autounattend.ps1
+++ src/w/packer/builders/iso/floppy/Autounattend.ps1
@@ -1,11 +1,14 @@
+Write-Host "Configure PowerShell"
 Set-ExecutionPolicy RemoteSigned -Force
+$ProgressPreference = 'SilentlyContinue'
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 Write-Host "Install Chocolatey"
 $env:chocolateyVersion = '0.10.15'
-Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
 
-Write-Host "Configure WinRM"
-netsh advfirewall firewall add rule name="Autounattend WinRM-HTTP" dir=in localport=5985 protocol=TCP action=block
+Write-Host "Install WinRM"
+netsh advfirewall firewall add rule name="WinRM-Install" dir=in localport=5985 protocol=TCP action=block
 
 Get-NetConnectionProfile | ForEach-Object { Set-NetConnectionProfile -InterfaceIndex $_.InterfaceIndex -NetworkCategory Private }
 
@@ -16,26 +19,23 @@ winrm set winrm/config/winrs '@{MaxMemoryPerShellMB="800"}'
 winrm set winrm/config/service '@{AllowUnencrypted="true"}'
 winrm set winrm/config/service/auth '@{Basic="true"}'
 winrm set winrm/config/client/auth '@{Basic="true"}'
-netsh advfirewall firewall add rule name="WinRM-HTTP" dir=in localport=5985 protocol=TCP action=allow
 
 net stop winrm
+netsh advfirewall firewall delete rule name="WinRM-Install"
 
-netsh advfirewall firewall delete rule name="Autounattend WinRM-HTTP"
-sc.exe config winrm start= auto
+netsh advfirewall firewall add rule name="WinRM-HTTP" dir=in localport=5985 protocol=TCP action=allow
+net start winrm
 
 Write-Host "Install OpenSSH"
-netsh advfirewall firewall add rule name="Autounattend SSH" dir=in localport=22 protocol=TCP action=block
+netsh advfirewall firewall add rule name="OpenSSH-Install" dir=in localport=22 protocol=TCP action=block
 
 choco install openssh -y --version 8.0.0.1 -params '"/SSHServerFeature"' # /PathSpecsToProbeForShellEXEString:$env:windir\system32\windowspowershell\v1.0\powershell.exe"'
+
+net stop sshd
+netsh advfirewall firewall delete rule name="OpenSSH-Install"
 
 $sshd_config = "$($env:ProgramData)\ssh\sshd_config"
 (Get-Content $sshd_config).Replace("Match Group administrators", "# Match Group administrators") | Set-Content $sshd_config
 (Get-Content $sshd_config).Replace("AuthorizedKeysFile", "# AuthorizedKeysFile") | Set-Content $sshd_config
 
-net stop sshd
-
-netsh advfirewall firewall delete rule name="Autounattend SSH"
-sc.exe config sshd start= auto
-
-Write-Host "Shut down"
-shutdown /r /t 10
+net start sshd
