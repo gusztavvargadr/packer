50ed6f5ebd509f8ba0280790a88683ed21d452d0
Gusztáv Varga <mail@gusztavvargadr.me>
2020-01-02 11:36:53 +0100

    Add initial version for AZP agent hosts #161

---
+++ src/core/packet/azp-agent-host.entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+terraform init
+terraform workspace select $1 || terraform workspace new $1
+
+terraform plan -out ./.terraform/terraform.tfplan
+terraform apply ./.terraform/terraform.tfplan
---
+++ src/core/packet/consul.agent.hcl
@@ -0,0 +1,7 @@
+{
+  "datacenter": "local",
+  "server": true,
+  "bootstrap_expect": 1,
+  "client_addr": "0.0.0.0",
+  "ui": true
+}
