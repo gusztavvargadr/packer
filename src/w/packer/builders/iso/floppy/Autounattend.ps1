ab77c122de687f3463ccec32275f9215ef70354a
Gusztáv Varga <mail@gusztavvargadr.me>
2020-01-02 11:36:22 +0100

    Clean up autounattend configuration #161

--- src/w/packer/builders/iso/floppy/Autounattend.ps1
+++ src/w/packer/builders/iso/floppy/Autounattend.ps1
@@ -9,9 +9,7 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-WebRequest https://choc
 
 Write-Host "Install WinRM"
 netsh advfirewall firewall add rule name="WinRM-Install" dir=in localport=5985 protocol=TCP action=block
-
 Get-NetConnectionProfile | ForEach-Object { Set-NetConnectionProfile -InterfaceIndex $_.InterfaceIndex -NetworkCategory Private }
-
 winrm quickconfig -q
 winrm quickconfig -transport:http
 winrm set winrm/config '@{MaxTimeoutms="1800000"}'
@@ -19,23 +17,21 @@ winrm set winrm/config/winrs '@{MaxMemoryPerShellMB="800"}'
 winrm set winrm/config/service '@{AllowUnencrypted="true"}'
 winrm set winrm/config/service/auth '@{Basic="true"}'
 winrm set winrm/config/client/auth '@{Basic="true"}'
-
 net stop winrm
 netsh advfirewall firewall delete rule name="WinRM-Install"
 
+Write-Host "Configure WinRM"
 netsh advfirewall firewall add rule name="WinRM-HTTP" dir=in localport=5985 protocol=TCP action=allow
 net start winrm
 
 Write-Host "Install OpenSSH"
 netsh advfirewall firewall add rule name="OpenSSH-Install" dir=in localport=22 protocol=TCP action=block
-
 choco install openssh -y --version 8.0.0.1 -params '"/SSHServerFeature"' # /PathSpecsToProbeForShellEXEString:$env:windir\system32\windowspowershell\v1.0\powershell.exe"'
-
 net stop sshd
 netsh advfirewall firewall delete rule name="OpenSSH-Install"
 
+Write-Host "Configure OpenSSH"
 $sshd_config = "$($env:ProgramData)\ssh\sshd_config"
 (Get-Content $sshd_config).Replace("Match Group administrators", "# Match Group administrators") | Set-Content $sshd_config
 (Get-Content $sshd_config).Replace("AuthorizedKeysFile", "# AuthorizedKeysFile") | Set-Content $sshd_config
-
 net start sshd
