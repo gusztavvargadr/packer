f86e1448d75db7db08488ab651ba988630d319c2
Gusztáv Varga <mail@gusztavvargadr.me>
2020-03-16 21:41:28 +0100

    Update Chef componens #173

--- src/w/chef/cookbooks/gusztavvargadr_packer_w/attributes/virtualbox_guest_additions.rb
+++ src/w/chef/cookbooks/gusztavvargadr_packer_w/attributes/virtualbox_guest_additions.rb
@@ -1,3 +1,10 @@
-default['gusztavvargadr_packer_w']['virtualbox_guest_additions'] = {
-  'version' => '6.1.4',
+default['gusztavvargadr_packer_w'] = {
+  'virtualbox_guest_additions' => {
+    'version' => '6.1.4',
+  },
+  'virtualbox_chocolatey_packages' => {
+    'virtio-drivers' => {
+      'version' => '0.1.173.301',
+    },
+  },
 }
