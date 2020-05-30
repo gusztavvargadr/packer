e52d9b69073b3af34fa2b2742a36ea738dfceac2
Gusztáv Varga <mail@gusztavvargadr.me>
2020-05-30 23:24:28 +0200

    Consolidate agent host setup #185

--- src/core/packet/azp-agent-host.install.ps1
+++ src/core/packet/azp-agent-host.install.ps1
@@ -32,7 +32,7 @@ wget https://vstsagentpackage.azureedge.net/agent/2.168.2/vsts-agent-win-x64-2.1
 [Environment]::SetEnvironmentVariable("VSTS_AGENT_INPUT_URL", "https://dev.azure.com/gusztavvargadr/", "User")
 [Environment]::SetEnvironmentVariable("VSTS_AGENT_INPUT_AUTH", "pat", "User")
 # [Environment]::SetEnvironmentVariable("VSTS_AGENT_INPUT_TOKEN", "Token42-", "User")
-# TODO Configure agents
+## TODO Configure agents
 
 # Workstation
 choco install -y googlechrome
@@ -43,7 +43,7 @@ choco install -y vscode
 choco install -y beyondcompare
 
 # Development
-choco install -y git --package-parameters="'/GitAndUnixToolsOnPath /NoAutoCrlf /NoShellIntegration'"
+choco install -y git --package-parameters="'/GitAndUnixToolsOnPath /NoAutoCrlf /NoShellIntegration /SChannel'"
 choco install -y poshgit
 choco install -y dotnetcore-sdk
 & 'C:\Program Files\dotnet\dotnet.exe' tool install Cake.Tool --global
@@ -57,21 +57,21 @@ choco install -y chef-workstation --version 0.18.3
 
 # choco install -y docker-desktop --version 2.2.0.5
 # choco install -y docker-desktop --version 2.3.0.3
-# TODO docker-machine
+## TODO docker-machine
 
 # choco install -y virtualbox --version 6.1.8
 # [Environment]::SetEnvironmentVariable("VAGRANT_DEFAULT_PROVIDER", "virtualbox", "User")
-# TODO extension pack
+## TODO extension pack
 
 # Get-WindowsOptionalFeature -Online | Where { $_.FeatureName -match "hyper" } | Where { $_.State -ne "Enabled" } | ForEach { Enable-WindowsOptionalFeature -Online -FeatureName $_.FeatureName -All -NoRestart }
-# New-VMSwitch -SwitchName HyperVNAT -SwitchType Internal
-# New-NetIPAddress -IPAddress 192.168.238.1 -PrefixLength 24 -InterfaceAlias "vEthernet (HyperVNAT)"
-# New-NetNat -Name HyperVNAT -InternalIPInterfaceAddressPrefix 192.168.238.0/24
+## New-VMSwitch -SwitchName HyperVNAT -SwitchType Internal
+## New-NetIPAddress -IPAddress 192.168.238.1 -PrefixLength 24 -InterfaceAlias "vEthernet (HyperVNAT)"
+## New-NetNat -Name HyperVNAT -InternalIPInterfaceAddressPrefix 192.168.238.0/24
 ## TODO Configure NAT external addresses
 # Get-WindowsOptionalFeature -Online | Where { $_.FeatureName -match "dhcp" } | Where { $_.State -ne "Enabled" } | ForEach { Enable-WindowsOptionalFeature -Online -FeatureName $_.FeatureName -All -NoRestart }
-# Add-DhcpServerv4Scope -Name "192.168.238.0/24" -StartRange 192.168.238.100 -EndRange 192.168.238.199 -SubnetMask 255.255.255.0 -LeaseDuration "04:00:00"
-# Set-DhcpServerv4OptionValue -ScopeId 192.168.238.0 -OptionId 3 -Value 192.168.238.1
-# Set-DhcpServerv4OptionValue -ScopeId 192.168.238.0 -OptionId 6 -Value 8.8.8.8,8.8.4.4
+## Add-DhcpServerv4Scope -Name "192.168.238.0/24" -StartRange 192.168.238.100 -EndRange 192.168.238.199 -SubnetMask 255.255.255.0 -LeaseDuration "04:00:00"
+## Set-DhcpServerv4OptionValue -ScopeId 192.168.238.0 -OptionId 3 -Value 192.168.238.1
+## Set-DhcpServerv4OptionValue -ScopeId 192.168.238.0 -OptionId 6 -Value 8.8.8.8,8.8.4.4
 ## TODO Static IP, gateway, DNS
 # [Environment]::SetEnvironmentVariable("VAGRANT_DEFAULT_PROVIDER", "hyperv", "User")
 # [Environment]::SetEnvironmentVariable("VAGRANT_PROVIDER_HYPERV_LINKED_CLONE", "true", "User")
