d19e36b97b43df897bef32a0f6e15d1ba7683d0a
Gusztáv Varga <mail@gusztavvargadr.me>
2020-06-22 21:04:09 +0200

    Fix Chef version specification and build cleanup #199

--- src/u1604/packer/provisioners/shell-prepare/scripts/chef.sh
+++ src/u1604/packer/provisioners/shell-prepare/scripts/chef.sh
@@ -4,7 +4,7 @@ case "$PACKER_BUILDER_TYPE" in
 virtualbox-iso|hyperv-iso|azure-arm)
     apt-get -y install p7zip-full;
 
-    curl -L https://omnitruck.chef.io/install.sh | bash -s -- -p chef -v 16.1.16
+    curl -L https://omnitruck.chef.io/install.sh | bash -s -- -P chef -v 16.1.16
     echo "CHEF_LICENSE=accept-silent" >> /etc/environment
 esac
 
