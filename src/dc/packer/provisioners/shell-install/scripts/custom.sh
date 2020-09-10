f7f16a5bbea10a5d5787bc4d62ad42bf32151c70
Gusztáv Varga <mail@gusztavvargadr.me>
2020-09-10 12:37:05 +0200

    Update Docker dependencies #216

--- src/dc/packer/provisioners/shell-install/scripts/custom.sh
+++ src/dc/packer/provisioners/shell-install/scripts/custom.sh
@@ -10,13 +10,6 @@ add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(
 apt-get update
 apt-get install -y docker-ce
 
-curl -L https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.27.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-curl -L https://raw.githubusercontent.com/docker/compose/1.26.2/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
-
-curl -L https://github.com/docker/machine/releases/download/v0.16.2/docker-machine-$(uname -s)-$(uname -m) -o /tmp/docker-machine
-install /tmp/docker-machine /usr/local/bin/docker-machine
-for i in docker-machine-prompt.bash docker-machine-wrapper.bash docker-machine.bash
-do
-  wget "https://raw.githubusercontent.com/docker/machine/v0.16.2/contrib/completion/bash/${i}" -P /etc/bash_completion.d
-done
+curl -L https://raw.githubusercontent.com/docker/compose/1.27.0/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
