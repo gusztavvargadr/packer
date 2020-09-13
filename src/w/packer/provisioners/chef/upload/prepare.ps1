1510ce0dd8b68c0b0ca79c0421ac79686b938d06
Gusztáv Varga <mail@gusztavvargadr.me>
2020-09-13 12:44:04 +0200

    Fix configuration for Windows Server 2019 #217

--- src/w/packer/provisioners/chef/upload/prepare.ps1
+++ src/w/packer/provisioners/chef/upload/prepare.ps1
@@ -1,10 +1,3 @@
-Write-Host "Install Chef Client"
-. { iwr -useb https://omnitruck.chef.io/install.ps1 } | iex; install -project chef -version 16.4.41
-[Environment]::SetEnvironmentVariable("CHEF_LICENSE", "accept-silent", "Machine")
-
-Write-Host "Install 7zip"
-choco install 7zip.portable -y
-
 Write-Host "Prepare cookbooks"
 7z x C:\tmp\packer\chef-upload\cookbooks.tar.gz -o"C:\tmp\packer\chef-upload" -aoa
 7z x C:\tmp\packer\chef-upload\cookbooks.tar -o"C:\tmp\packer\chef-upload" -aoa
