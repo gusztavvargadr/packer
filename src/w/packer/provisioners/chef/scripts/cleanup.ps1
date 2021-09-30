e900c9a84133ebf92699e2d46a5d6ab0cb866ad2
rjhornsby <github.com@w.ktzr.net>
2021-09-30 09:55:09 -0500

    Remove chef client at end of build

--- src/w/packer/provisioners/chef/scripts/cleanup.ps1
+++ src/w/packer/provisioners/chef/scripts/cleanup.ps1
@@ -1,2 +1,6 @@
 Write-Host "Clean up"
 Remove-Item -Recurse -Force C:/Windows/Temp/chef
+
+# Remove chef client, or it will cause problems later when kitchen tries to install cinc.
+$chef_client = Get-WmiObject -Class Win32_Product | Where-Object{$_.Name.StartsWith("Chef Infra")}
+$chef_client.Uninstall()
