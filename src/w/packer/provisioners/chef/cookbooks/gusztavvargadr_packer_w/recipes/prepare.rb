b93f01274490cee15c805361ff631d2902e40a36
Gusztáv Varga <mail@gusztavvargadr.me>
2022-01-23 03:33:39 +0100

    Fix VMware Tools installation #168

--- src/w/packer/provisioners/chef/cookbooks/gusztavvargadr_packer_w/recipes/prepare.rb
+++ src/w/packer/provisioners/chef/cookbooks/gusztavvargadr_packer_w/recipes/prepare.rb
@@ -104,16 +104,16 @@ if vbox?
       action :dismount
     end
   end
+end
 
-  if vmware?
-    reboot 'vmware' do
-      action :nothing
-    end
+if vmware?
+  reboot 'vmware' do
+    action :nothing
+  end
 
-    chocolatey_package 'vmware-tools' do
-      returns [0, 2, 3010]
-      action :install
-      notifies :request_reboot, 'reboot[vmware]'
-    end
+  chocolatey_package 'vmware-tools' do
+    returns [0, 2, 3010]
+    action :install
+    notifies :request_reboot, 'reboot[vmware]'
   end
 end
