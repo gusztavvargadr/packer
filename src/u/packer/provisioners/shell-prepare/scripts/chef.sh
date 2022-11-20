008ffb478d7820b2c8a741843f60be07881f24ee
Gusztáv Varga <mail@gusztavvargadr.me>
2022-11-20 13:50:37 +0100

    Update core settings #362

--- src/u/packer/provisioners/shell-prepare/scripts/chef.sh
+++ src/u/packer/provisioners/shell-prepare/scripts/chef.sh
@@ -2,6 +2,6 @@
 
 case "$PACKER_BUILDER_TYPE" in
 virtualbox-iso|hyperv-iso|azure-arm)
-    curl -L https://omnitruck.chef.io/install.sh | bash -s -- -P chef -v 17.10.3
+    curl -L https://omnitruck.chef.io/install.sh | bash -s -- -P chef -v 18.0.185
     echo "CHEF_LICENSE=accept-silent" >> /etc/environment
 esac
