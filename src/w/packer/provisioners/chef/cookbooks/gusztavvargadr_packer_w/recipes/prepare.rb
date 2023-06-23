010df2b1b497401e9059cc9bd36dc588cd74af2b
Gusztáv Varga <mail@gusztavvargadr.me>
2023-06-23 15:30:22 +0200

    Fix Chocolatey usage for 2.0

--- src/w/packer/provisioners/chef/cookbooks/gusztavvargadr_packer_w/recipes/prepare.rb
+++ src/w/packer/provisioners/chef/cookbooks/gusztavvargadr_packer_w/recipes/prepare.rb
@@ -77,7 +77,7 @@ remote_file sdelete_executable_target do
 end
 
 if vbox?
-  vbox_guest_additions_installed = powershell_out('choco list -li').stdout.downcase.include? 'virtualbox'
+  vbox_guest_additions_installed = powershell_out('choco list -i').stdout.downcase.include? 'virtualbox'
   unless vbox_guest_additions_installed
     reboot 'vbox' do
       action :nothing
