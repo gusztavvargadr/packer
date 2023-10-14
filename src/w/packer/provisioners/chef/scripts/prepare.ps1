11bd500546e8ebee9bf5dee99e7a935cc81fcc9c
Gusztáv Varga <mail@gusztavvargadr.me>
2023-10-14 23:29:55 +0200

    Update core versions

--- src/w/packer/provisioners/chef/scripts/prepare.ps1
+++ src/w/packer/provisioners/chef/scripts/prepare.ps1
@@ -5,5 +5,5 @@ If ((Get-Command "chef-client" -ErrorAction Ignore).Count -gt 0) {
 }
 
 Write-Host "Install Chef Client"
-. { iwr -useb https://omnitruck.chef.io/install.ps1 } | iex; install -project chef -version 18.2.7
+. { iwr -useb https://omnitruck.chef.io/install.ps1 } | iex; install -project chef -version 18.3.0
 [Environment]::SetEnvironmentVariable("CHEF_LICENSE", "accept-silent", "Machine")
