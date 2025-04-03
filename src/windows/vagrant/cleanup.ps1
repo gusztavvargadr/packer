5ec19c94e2393ff45aa7c8f5ebefd909cb045055
Gusztáv Varga <mail@gusztavvargadr.me>
2025-04-03 15:32:20 +0200

    Fix Ubuntu and Windows provisioning

--- src/windows/vagrant/cleanup.ps1
+++ src/windows/vagrant/cleanup.ps1
@@ -1,2 +1,2 @@
-Get-AppxPackage -Name "*OneDriveSync*" | Remove-AppxPackage
-Get-AppxPackage -Name "*MicrosoftEdge*" | Remove-AppxPackage
+# Get-AppxPackage -Name "*OneDriveSync*" | Remove-AppxPackage
+# Get-AppxPackage -Name "*MicrosoftEdge*" | Remove-AppxPackage
